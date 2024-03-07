966d6452aeab381269a707c667c4b7907bed9fa3
unknown <etimubongabasi@gmail.com>
2024-03-07 03:24:38 +0000

    more model rendering and pdf creation

--- citisoft/views.py
+++ citisoft/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http.response import HttpResponse
 from django.template import loader
 from .models import *
 from .models import Vendorr
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import landscape, letter  # Assuming letter size for the PDF
+from reportlab.lib import colors
+from reportlab.lib.units import inch
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import Paragraph
+
+#import weasyprint
+#from weasyprint import HTML
 
 # Create your views here.
 # Function-based view for index page
@@ -33,26 +42,85 @@ def home(request):   # Request handler
 
 # Function-based view for choice page
 def choice(request, vendor_id):   # Request handler
-      vendor = get_object_or_404(Vendorr, pk=vendor_id)  # Get the requested product or display a 404 error if it
+      vendor = get_object_or_404(Vendorr, id=vendor_id)  # Get the requested product or display a 404 error if it
       context = {'VENDOR': vendor}   # Initialize context dictionary
    # Render choice.html with context
       return render(request, 'citisoft/user/choice.html', context)
 
 # Function-based view for cart page
-def cart(request):   # Request handler
-  # if request.user.is_authenticated:  # If user is
-   #   client = request.user.client  # authenticated, get the corresponding Client object
-   #   vendor, created = Vendorr.objects.get_or_create(client=client, complete = False)  # and create a new Vendor associated to this Client
-       
-   context = {}   # Initialize context dictionary
+def cartOperation(request, vendor_id):   # Request handler
+   company = Vendorr.objects.get(id=vendor_id)
+   # Save the company name to the database
+   Saved_vendor.objects.create(name=company.name)    
+   #context = {}   # Initialize context dictionary
    # Render cart.html with context
+   return redirect('choice', vendor_id=vendor_id) 
+
+def cart(request):   # Request handler
+   vendor = Vendorr.objects.all()
+   saved =  Saved_vendor.objects.all()
+   context = {'Vendors': vendor, 'saved_vend': saved}   # Initialize context dictionary
+   # Render products.html with context
    return render(request, 'citisoft/user/cart.html', context)
 
 
-def category(request):   # Request handler
-   context = {}   # Initialize context dictionary
-   # Render category.html with context
-   return render(request, 'citisoft/user/category.html', context)
+
+def pdf_view(request, vendor_id):
+    # Retrieve the vendor object from the database
+    company = get_object_or_404(Vendorr, id=vendor_id)
+
+    # Create a PDF document using ReportLab
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename="{company.name}_details.pdf"'
+
+    p = canvas.Canvas(response, pagesize=landscape(letter))
+
+    # Set initial y-position and line height
+    x_position = 10
+    y_position = 550
+    line_height = 25
+
+    # Define paragraph style for multiline text
+    paragraph_style = ParagraphStyle(name='Normal', fontSize= 8.5 , leading=12)
+
+    # Write company details to PDF
+    p.drawString(50, y_position, f"Company Name: {company.name}")
+    y_position -= line_height
+
+    # Wrap and draw description
+    description_text = Paragraph(company.description, paragraph_style)
+    description_text.wrapOn(p, 700, 850)
+    description_text.drawOn(p, x_position, y_position)
+    y_position -= description_text.height
+    # Write other company details
+    details = [
+        ("Email", company.email),
+        ("Genre", company.genre),
+        ("Date Established", company.date_est),
+        ("Location", company.location),
+        ("Address", company.address),
+        ("Contact Telephone", company.contact_tel),
+        ("Number of Employees", company.num_of_eployees),
+        ("International Professional Services", company.Int_pro_services),
+        ("Business Areas", company.business_areas),
+        ("Client Types", company.client_types),
+        ("Cloud or Native", company.cloud_or_native),
+        ("Additional Information", company.added_info),
+    ]
+
+    for detail_name, detail_value in details:
+        detail_text = Paragraph( f"{detail_name}: {detail_value}", paragraph_style)
+        detail_text.wrapOn(p, 700, 800)
+        detail_text.drawOn(p, x_position, y_position)
+        y_position -= detail_text.height
+        y_position -= line_height
+
+    p.showPage()
+    p.save()
+
+    return response
+
+   #return render(request, 'citisoft/user/category.html', context)
 
 
 def delete(request):   # Request handler
